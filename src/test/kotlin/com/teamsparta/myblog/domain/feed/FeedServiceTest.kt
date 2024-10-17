7e7e04530e2989600b96f3587b85eb4aaff9580c
gooddle <vptl6559@gmail.com>
2024-10-17 15:51:38 +0900

    :tada: add ci.yml

--- src/test/kotlin/com/teamsparta/myblog/domain/feed/FeedServiceTest.kt
+++ src/test/kotlin/com/teamsparta/myblog/domain/feed/FeedServiceTest.kt
@@ -6,8 +6,10 @@ import com.teamsparta.myblog.domain.exception.ModelNotFoundException
 import com.teamsparta.myblog.domain.feed.dto.FeedRequest
 import com.teamsparta.myblog.domain.feed.model.Feed
 import com.teamsparta.myblog.domain.feed.model.FeedCategory
+
 import com.teamsparta.myblog.domain.feed.repository.FeedRepository
 import com.teamsparta.myblog.domain.feed.service.FeedServiceImpl
+import com.teamsparta.myblog.domain.user.model.Role
 import com.teamsparta.myblog.domain.user.model.User
 import com.teamsparta.myblog.domain.user.repository.UserRepository
 import io.kotest.assertions.throwables.shouldThrow
@@ -28,7 +30,7 @@ class FeedServiceTest : BehaviorSpec({
         When("특정 feed를 조회하면") {
             Then("ModelNotFoundException 발생 .") {
                 shouldThrow<ModelNotFoundException> {
-                    feedService.getFeedById(2)
+                    feedService.getFeedById(1)
                 }
             }
         }
@@ -36,41 +38,43 @@ class FeedServiceTest : BehaviorSpec({
 
 
 })
-// ){
-//    companion object{
-//        private const val FEED_ID =1L
-//        private const val USER_ID = 1L
-//        private const val COMMENT_ID = 1L
-//        private val comments: MutableList<Comment> = mutableListOf()
-//        private val user =User(
-//            id = USER_ID,
-//            email = "test@test.com",
-//            password = "tesT12!@",
-//        )
-//        private val deletedFeed = Feed(
-//            id = FEED_ID,
-//            title = "test1",
-//            content = "test1",
-//            createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 0),
-//            updatedAt = LocalDateTime.of(2021, 1, 2, 1, 1, 0),
-//            deleted = true,
-//            deletedAt = null,
-//            user = user,
-//            feedCategory = FeedCategory.IOS
-//        )
-//        private val comment = Comment(
-//            id =COMMENT_ID,
-//            title = "testcomment1",
-//            content = "testcomment1",
-//            createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 0),
-//            updatedAt = LocalDateTime.of(2021, 1, 2, 1, 1, 0),
-//            feed = deletedFeed,
-//            user = user
-//        )
-//
-//        private val updateFeedRequest = FeedRequest(
-//            title ="testtitle",
-//            content = "testcontent",
-//        )
-//    }
-//}
+ {
+    companion object{
+        private const val FEED_ID =1L
+        private const val USER_ID = 1L
+        private const val COMMENT_ID = 1L
+        private val comments: MutableList<Comment> = mutableListOf()
+        private val user = User(
+            id = USER_ID,
+            email = "test@test.com",
+            password = "tesT12!@",
+            role = Role.USER
+        )
+        private val deletedFeed = Feed(
+            id = FEED_ID,
+            title = "test1",
+            content = "test1",
+            createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 0),
+            updatedAt = LocalDateTime.of(2021, 1, 2, 1, 1, 0),
+            deleted = true,
+            deletedAt = null,
+            user = user,
+            feedCategory = FeedCategory.IOS
+        )
+        private val comment = Comment(
+            id =COMMENT_ID,
+            title = "testcomment1",
+            content = "testcomment1",
+            createdAt = LocalDateTime.of(2021, 1, 1, 1, 1, 0),
+            updatedAt = LocalDateTime.of(2021, 1, 2, 1, 1, 0),
+            feed = deletedFeed,
+            user = user
+        )
+
+        private val updateFeedRequest = FeedRequest(
+            title ="testtitle",
+            content = "testcontent",
+        )
+    }
+}
+
